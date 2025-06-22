00f744560e2cab95f6878830fbaaa974e2a62624
Anmol Kumar <anmolkumar.cod01@gmail.com>
2025-06-22 23:28:35 +0530

    added user routes , cloudinary and multer for taking image from the user

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import  dotenv from 'dotenv'
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import {connectDB} from './lib/db.js'
 
 dotenv.config()
 
 
 const app = express()
+app.use(express.json())
+app.use(cookieParser())
 
 import authRoutes from './routes/auth.route.js'
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,17 +1,30 @@
 import express from "express";
+import { upload } from "../middlewares/multer.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 import { 
     signup,
     login,
-    logout
+    logout,
+    updateProfile,
+    checkLoggedIn
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+// routes
 router.post("/signup", signup);
-
 router.post("/login",login);
-
 router.post("/logout", logout);
-
+router.route("/update-profile").post(
+    upload.fields([
+        {
+            name: "profilePic",
+            maxCount: 1
+        }
+    ]),
+    protectRoute,
+    updateProfile
+);
+router.get("/check-loggedIn" , protectRoute, checkLoggedIn);
 
 export default router;
