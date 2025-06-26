7b1ce48fabfa1c677c79047db04e77a807abb675
Anmol Kumar <anmolkumar.cod01@gmail.com>
2025-06-27 04:13:05 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import  dotenv from 'dotenv'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import {connectDB} from './lib/db.js'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -9,6 +10,10 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}))
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
@@ -16,6 +21,7 @@ import messageRoutes from './routes/message.route.js'
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages",messageRoutes)
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port " + (process.env.PORT || 5000));
