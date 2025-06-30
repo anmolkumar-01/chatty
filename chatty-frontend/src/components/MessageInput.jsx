fd33c3aa6d65f513bf25a7a0b771316e6e241567
Anmol Kumar <anmolkumar.cod01@gmail.com>
2025-06-30 16:40:56 +0530

    still working on message input component

--- chatty-frontend/src/components/MessageInput.jsx
+++ chatty-frontend/src/components/MessageInput.jsx
@@ -1,4 +1,5 @@
 import React, { useState,useRef } from 'react'
+import {X, Send, Image} from 'lucide-react'
 
 const MessageInput = () => {
 
@@ -6,8 +7,97 @@ const MessageInput = () => {
   const [imagePreview , setImagePreview] = useState("")
   const fileInputRef = useRef(null)
 
+  const handleImageChange = (e) => {}
+
+  const removeImage = () => {}
+
+  const handleSendMessage = async(e) => {}
+
   return (
-    <div>MessageInput</div>
+
+    <div className="p-4 w-full">
+
+      {/*  -------- mini image preview icon ------------ */}
+      {imagePreview && (
+
+        <div className="mb-3 flex items-center gap-2">
+
+          <div className="relative">
+
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+
+          </div>
+
+        </div>
+        
+      )}
+
+
+      {/* ------------ lower inputs -------------- */}
+      <form onSubmit={handleSendMessage} className="flex items-center gap-2">
+
+        <div className="flex-1 flex gap-2">
+
+          {/* text input */}
+          <input
+            type="text"
+            className="w-full input input-bordered rounded-lg input-sm sm:input-md"
+            placeholder="Type a message..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          {/* image button */}
+          <div>
+              {/* file input button functionality - hidden*/}
+            <input
+              type="file"
+              accept="image/*"
+              className="hidden"
+              ref={fileInputRef}
+              onChange={handleImageChange}
+            />
+
+              {/* file input icon vutton*/}
+            <button
+              type="button"
+              className={`hidden sm:flex btn btn-circle
+                      ${imagePreview ? "text-emerald-500" : "text-zinc-400"}`}
+              onClick={() => fileInputRef.current?.click()}
+            >
+              <Image size={20} />
+            </button>
+
+          </div>
+          
+        </div>
+
+          {/*  send message button */}
+        <button
+          type="submit"
+          className="btn btn-sm btn-circle"
+          disabled={!text.trim() && !imagePreview}
+        >
+          <Send size={22} />
+        </button>
+
+      </form>
+
+    </div>
+
   )
 }
 
