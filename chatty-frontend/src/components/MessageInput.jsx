77db6cee7cf59e4fe8bb5d75309815f7417b2cf5
Anmol Kumar <anmolkumar.cod01@gmail.com>
2025-06-30 16:00:55 +0530

    adding messageInput

--- chatty-frontend/src/components/MessageInput.jsx
+++ chatty-frontend/src/components/MessageInput.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState,useRef } from 'react'
 
 const MessageInput = () => {
+
+  const [text , setText] = useState("")
+  const [imagePreview , setImagePreview] = useState("")
+  const fileInputRef = useRef(null)
+
   return (
     <div>MessageInput</div>
   )
