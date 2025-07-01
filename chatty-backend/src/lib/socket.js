64050a63811769f4e2f8df1c0f4df60a05ead83d
Anmol Kumar <anmolkumar.cod01@gmail.com>
2025-07-01 20:47:05 +0530

    update CORS origin to correct production URL

--- chatty-backend/src/lib/socket.js
+++ chatty-backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors: {
-        origin: ['http://localhost:5173' , 'https://chatty-backend-p005.onrender.com']
+        origin: ['http://localhost:5173' , 'https://chatty-frontend-fq81.onrender.com']
     }
 });
 
